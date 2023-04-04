c59da4d31466112b2229743122710e62e382336c
Emad Ahmad <emxdahmad@gmail.com>
2023-04-04 20:06:45 +0530

    Update run-opengl.bat

--- run-opengl.bat
+++ run-opengl.bat
@@ -1,9 +1,17 @@
-echo "Running"
+:: Changing working directory to the bat file directory
+cd "%~dp0"
+
+:: Compiling object file 
+echo "Compiling"
 g++ -c -o main.o main.cpp -I"C:\Program Files\Common Files\freeglut\include"   
 cls
-echo "Compiling Graphic"
+
+:: Linking 
+echo "Linking"
 g++ -o main.exe main.o -L"C:\Program Files\Common Files\freeglut\lib" -lfreeglut -lopengl32 -lglu32 
-rm main.o
+del main.o
 cls
+
+:: Execute 
 echo "App Running"
 main.exe
